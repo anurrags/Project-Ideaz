3b0c8d842a578f5017b84695652fe6f3e2f9eac9
Anurag Sharma <iamanurag0501@gmail.com>
2024-08-04 12:45:55 +0530

    changed footer margin

--- client/src/pages/ideaPage/IdeaPage.tsx
+++ client/src/pages/ideaPage/IdeaPage.tsx
@@ -4,7 +4,7 @@ const IdeaPage = () => {
   return (
     <div>
       <IdeaForm />
-      <footer style={{ textAlign: "center" }}>
+      <footer style={{ textAlign: "center", margin: "5rem 0 2rem 0" }}>
         &#169; {new Date(Date.now()).getFullYear()} Anurag Sharma
       </footer>
     </div>
